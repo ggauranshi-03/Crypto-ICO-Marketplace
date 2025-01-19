95e382c4379c3032d664315761fb12267a1b1015
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2025-01-19 13:02:07 +0530

    Update ERC20.sol

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzepplin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Token is ERC20 {
-    constructor() ERC20("AnantJain", "AJ"){
+    constructor() ERC20("GauranshiGupta", "GG"){
         _mint(msg.sender, 50000**8);
     }
 }
